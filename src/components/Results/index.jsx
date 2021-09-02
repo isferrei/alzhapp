40ef7caa7d2e976ebae171482da2fa30136b6493
Isabela Carvalho <isabelad-carvalho@hotmail.com>
2021-09-01 21:57:30 -0300

    Update

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -9,7 +9,7 @@ import {
   Percentis,
   PercentisCard,
   PrintButton,
-  Column
+  Column,
 } from "./styles";
 import { CognitiveTestContext } from "../../context/cognitiveTestContext";
 import { OctDataContext } from "../../context/octDataContext";
@@ -21,60 +21,71 @@ function Results() {
   const octData = React.useContext(OctDataContext);
   const resultsRef = React.useRef();
 
-  const handlePrint = useReactToPrint({
-    content: () => resultsRef.current,
-  });
+  function handlePrint(id) {
+    const result = document.getElementById("results").innerHTML;
+    var myWindow = window.open("", "Results", "height=400,width=600");
+    myWindow.document.write("<html><head>");
+    myWindow.document.write("</head><body>");
+    myWindow.document.write(result);
+    myWindow.document.write("</body></html>");
+    myWindow.document.close(); // necessary for IE >= 10
+
+    myWindow.onload = function () {
+      myWindow.focus();
+      myWindow.print();
+    };
+  }
 
   return (
     <>
-      <Container ref={resultsRef}>
+      <Container id="results" ref={resultsRef}>
         <center>
           <h4>Results / Summary:</h4>
         </center>
         <Form>
           <Box>
-            <label>Physician:</label>{' '}
+            <label>Physician:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.physician}</p>
           </Box>
           <Box>
-            <label>Patient:</label>{' '}
+            <label>Patient:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.patient}</p>
           </Box>
           <Box>
-            <label>ID number:</label>{' '}
+            <label>ID number:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.idNumber}</p>
           </Box>
           <Box>
             <label>Sex:</label> <p>{cognitiveTestData.cognitiveTest.sex}</p>
-            <label className='label-age'> Age:</label>
+            <label className="label-age"> Age:</label>
             <p>{cognitiveTestData.cognitiveTest.age}</p>
           </Box>
           <Box>
-            <label>Diagnosis:</label>{' '}
+            <label>Diagnosis:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.diagnosis}</p>
           </Box>
           <Box>
-            <label>Cognitive test A:</label>{' '}
+            <label>Cognitive test A:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.cognitiveTestA}</p>
           </Box>
           <Box>
-            <label>Score value:</label>{' '}
+            <label>Score value:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.scoreA}</p>
           </Box>
           <Box>
-            <label>Cognitive test B:</label>{' '}
+            <label>Cognitive test B:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.cognitiveTestB}</p>
           </Box>
           <Box>
-            <label>Score value:</label>{' '}
+            <label>Score value:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.scoreB}</p>
           </Box>
           <Box>
-            <label>Ocular disease:</label>{' '}
+            <label>Ocular disease:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.ocularDisease}</p>
           </Box>
           <Box>
-            <label>OCT Model:</label>{' '}
+            <label>OCT Model:</label>{" "}
             <p>{cognitiveTestData.cognitiveTest.octModel}</p>
           </Box>
         </Form>
@@ -227,7 +238,7 @@ function Results() {
               <tr></tr>
               <tr>
                 <td>
-                  {' '}
+                  {" "}
                   {octData.gclIplOsPerc ? octData.gclIplOsPerc : <Percentis />}
                 </td>
               </tr>
@@ -277,8 +288,8 @@ function Results() {
           </PercentisCard>
         </Row>
       </Container>
-      <PrintButton onClick={handlePrint}>
-        <img src={print} alt='print icon' width='20px' height='20px' />
+      <PrintButton onClick={() => handlePrint("results")}>
+        <img src={print} alt="print icon" width="20px" height="20px" />
         Print / Save
       </PrintButton>
     </>
